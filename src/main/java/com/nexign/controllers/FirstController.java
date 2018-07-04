14c9d7e4ffe41afcd3cad8f602289befee4705ca
Gennady.Savinov <Gennady.Savinov@billing.ru>
2018-07-04 14:21:15 +0400

    feat(*): Создание проекта с помощью spring-boot

---
+++ src/main/java/com/nexign/controllers/FirstController.java
@@ -0,0 +1,15 @@
+package com.nexign.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FirstController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String hello() {
+        return "Hello World";
+    }
+}
