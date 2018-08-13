edcccf6bfecfda8ac4398244f10b3c91577faaff
Gennady.Savinov <Gennady.Savinov@billing.ru>
2018-08-13 16:55:12 +0400

    feat(*): Добавлен паттерн билдер

--- src/main/java/com/nexign/model/patterns/builder/PatternBuilder.java
+++ src/main/java/com/nexign/model/patterns/builder/PatternBuilder.java
@@ -1,4 +1,24 @@
 package com.nexign.model.patterns.builder;
 
 public class PatternBuilder {
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public PatternBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public PatternBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
 }
